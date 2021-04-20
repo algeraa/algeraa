c66876a4dcaf60fabad7977a3f53b13945266470
ale959vg <79913243+ale959vg@users.noreply.github.com>
2021-04-20 21:46:10 +0200

    Vuelvo a añadir

---
+++ pruebasPersonaje/js/escena3.js
@@ -0,0 +1,26 @@
+export default class Escena3 extends Phaser.Scene {
+	constructor(){
+		super({key: "Escena3"});
+	}
+	preload()
+	{
+
+	}
+	create()
+	{
+		let graphics = this.add.graphics();
+
+		graphics.fillStyle(0xff9999, 1);
+
+		graphics.fillRect(100,200,600,300);
+		graphics.fillRect(300,100,100,100);
+
+		this.add.text(320, 110, "C", {font: "96px Courier", fill: "#000000"});
+
+		
+	}
+	update(time, delta)
+	{
+
+	}
+}
