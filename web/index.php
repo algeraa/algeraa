794796be590ff741affb2b0ac5b727536a01a0ab
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-18 14:09:35 +0200

    Login

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'/>	
+<title>Login</title>
+
+</head>
+<body>
+ 
+	<form action="login.proc.php" method="POST">
+		Usuario: <input type="text" name="user" required=""><br>
+		Contraseña: <input type="password" name="contra" required=""><br>	
+		<input type="submit" name="Enviar" value="Enviar">
+	</form>
+</body>
+</html>
