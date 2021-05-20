beb07e7febd13a28326afc86230204ca8179de99
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-20 11:05:43 +0200

    Update registro.php

--- web/php/registro.php
+++ web/php/registro.php
@@ -5,7 +5,12 @@
 		<title>Login</title>
 	</head>
 	<body>
-hola
+		<form action="registroSQL.php" method="POST">
+			Usuario: <input type="text" name="user" required=""><br>
+			Contraseña: <input type="password" name="contra" required=""><br>
+			Confirmar contraseña: <input type="password" name="contraCon" required=""><br>
+			<input type="submit" name="Registrar" value="Registrar">
+		</form>
 	
 	</body>
 </html>
