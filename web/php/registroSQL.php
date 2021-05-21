9c15cf77ffacbbff442c2f31f5893e970b67d38e
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-21 08:55:50 +0200

    Update registroSQL.php

--- web/php/registroSQL.php
+++ web/php/registroSQL.php
@@ -1,19 +1,20 @@
 <?php
+echo("1");
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- sslmode=require ????
 }
-
+echo("2");
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
-
+echo("3");
   $usuario = $_POST['user'];
   $cont = $_POST['contra'];
   $contCon = $_POST['contraCon'];
-
+echo("4");
   $sql = INSERT INTO public."JUGADORES"
 (usuario, contrasenia, codigo_jugadores)
 VALUES('$usuario', '$cont', 1);
-
+echo("5");
     $conec=pg_query($pg_conn, $sql);
     header("Location: ../juego.html");
     
