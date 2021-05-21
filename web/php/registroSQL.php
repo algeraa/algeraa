e9f703ee5bcbd9305c6f5d19424e34aafc692c6a
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-21 08:43:57 +0200

    Update registroSQL.php

--- web/php/registroSQL.php
+++ web/php/registroSQL.php
@@ -1,6 +1,10 @@
 <?php
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- sslmode=require ????
+}
 
-  include 'conexion.php';
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
 
   $usuario = $_POST['user'];
   $cont = $_POST['contra'];
