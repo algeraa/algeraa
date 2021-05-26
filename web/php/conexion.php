cbc9b87be88bfc4bdd84bd45847622bf1eda2c41
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-26 11:21:01 +0200

    eqwf

--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 # Establecemos la conexión
-$conn = mysqli_connect('localhost','root','','algeraa');
+$conn = mysqli_connect('sql11.freemysqlhosting.net','sql11415001','Si6LTSDhJE','algeraa');
 
 # Testeamos que funciona
 //$result = mysqli_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
