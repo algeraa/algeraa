46ad844ef58cf47811c18ee0902f5c63703da176
algeraa <81708196+algeraa@users.noreply.github.com>
2021-05-26 11:28:24 +0200

    Update conexion.php

--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 # Establecemos la conexión
-$conn = mysqli_connect('sql11.freemysqlhosting.net','sql11415001','Si6LTSDhJE','algeraa');
+$conn = mysqli_connect('sql11.freemysqlhosting.net','sql11415001','Si6LTSDhJE','sql11415001');
 
 # Testeamos que funciona
 //$result = mysqli_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
